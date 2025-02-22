e4b810ba8c740bd1a306dd0d5aab4d64a91cd93d
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-22 15:21:11 +0100

    text

--- script.js
+++ script.js
@@ -21,28 +21,82 @@ const utils = {
     main.innerHTML = content;
     document.querySelector(".btn-container").innerHTML = btn;
   },
+  handleEventMinutes: function () {
+     document.querySelectorAll('input[type="number"]').forEach((input)=>{
+     input.addEventListener('input', (e)=>{
+      console.log(e)
+      exerciceArray.map((exo)=>{
+        console.log("test")
+        if(exo.pic==e.target.id){
+            console.log("yes")
+            exo.min=parseInt(e.target.value);
+            console.log(exerciceArray);
+        }
+      })
+
+    })
+   
+    })
+
+  },
+  handleEventArrow: function(){
+    document.querySelectorAll(".arrow").forEach((arrow)=>{
+        arrow.addEventListener("click", (e)=>{
+            let position=0;
+            console.log(e);
+            exerciceArray.map((exo)=>{
+                if(exo.pic==e.target.dataset.pic && position!==0){
+
+                    [exerciceArray[position], exerciceArray[position-1]]=[exerciceArray[position-1], exerciceArray[position]]
+                    //  console.log(exerciceArray)
+                     page.lobby()
+                //   return  console.log("yes")
+                }else{
+                    position++;
+                    // console.log(position)
+                }
+            })
+        })
+    })
+  }
 };
 
 const page = {
   lobby: function () {
+    let mapArray = exerciceArray
+      .map(
+        (exo) =>
+ `<li>
+          <div class="card-header">
+          <input type="number" id=${exo.pic} min="10" max="10"  value=${exo.min}>
+           <span>min</span>
+           <img src="/img/${exo.pic}.jpg" alt="exercice" />
+           <i class="fas fa-arrow-alt-circle-left arrow" data-pic=${exo.pic}></i>
+           <i class="fas fa-times-circle deleteBtn" data-pic=${exo.pic}></i>
+     </div>
+
+</li>`
+      )
+      .join("");
     utils.pageContent(
       "Parametrage <i id='reboot' class='fas fa-undo'></i>",
-    ExercicesArray,
+      "<ul>" + mapArray + "</ul>",
       "<button id='start'>Start <i class='far fa-play-circle'></i></button>"
     );
+     utils.handleEventMinutes();
+     utils.handleEventArrow();
+     utils.deleteItem()
   },
 
   routine: function () {
-   utils.pageContent(    "Routine","Exercice avec chrono", null );
-
- 
+    utils.pageContent("Routine", "Exercice avec chrono", null);
   },
 
   finish: function () {
     utils.pageContent(
-    "c'est termine",
-    "<button id='start'>Recommencer</button>",
-    "<button id='reboot' class='Renitialiser<i class='fas fa-times-circle'></i></button>"
+      "c'est termine",
+      "<button id='start'>Recommencer</button>",
+      "<button id='reboot' class='Renitialiser<i class='fas fa-times-circle'></i></button>"
     );
   },
 };
